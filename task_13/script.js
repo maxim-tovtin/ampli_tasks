eba2900fab6891bd6627b2b3a81f15a070a8ee5f
maxim-tovtin <MaksimTovtin3@gmail.com>
2021-03-05 15:25:19 +0200

    add task13

--- task_13/script.js
+++ task_13/script.js
@@ -1,1 +1,11 @@
-var answer = prompt('Ваше имя?');
+var test = prompt("Тест", ''); //
+
+alert('Роботает ' + test + '!'); //
+
+var da = confirm("Я справился?"); //
+
+alert( da ); //
+
+var years = prompt('Как вас зовут?'); //
+
+alert('Привет ' + years + ' приятно познакомится!') //
